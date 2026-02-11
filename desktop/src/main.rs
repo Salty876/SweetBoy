de885b2c79d8c0450ebbc7a878f1e16ab57431ba
Salty876 <salabdoulaye876@gmail.com>
2026-02-10 19:24:05 -0700

    work on these tests at home

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -2,14 +2,26 @@ use gb_core::cpu::Cpu;
 
 
 pub fn main() {
+    let rom = std::fs::read("../blaarg/cpu_instr/individual/01-special.gb").unwrap();
+
     let mut cpu = Cpu::new();
+    cpu.load_rom(&rom);
+
+    // power-up state
+    cpu.regs.set_af(0x01B0);
+    cpu.regs.set_bc(0x0013);
+    cpu.regs.set_de(0x00D8);
+    cpu.regs.set_hl(0x014D);
+    cpu.sp = 0xFFFE;
+    cpu.pc = 0x0100;
+
     loop {
-        
         cpu.step();
         if cpu.halted {
             break;
         }
     }
+
 }
 
 
--- desktop/target/debug/deps/gb_core-26e4a9770836d5d3.d
+++ desktop/target/debug/deps/gb_core-26e4a9770836d5d3.d
@@ -1,9 +1,12 @@
-/Users/salabdoulaye/LebronBoy/desktop/target/debug/deps/libgb_core-26e4a9770836d5d3.rmeta: /Users/salabdoulaye/LebronBoy/gb_core/src/lib.rs /Users/salabdoulaye/LebronBoy/gb_core/src/gb/mod.rs /Users/salabdoulaye/LebronBoy/gb_core/src/gb/cpu.rs
+/Users/salabdoulaye/LebronBoy/desktop/target/debug/deps/libgb_core-26e4a9770836d5d3.rmeta: /Users/salabdoulaye/LebronBoy/gb_core/src/lib.rs /Users/salabdoulaye/LebronBoy/gb_core/src/bus/mod.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/mod.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/registers.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/instructions.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/execute.rs
 
-/Users/salabdoulaye/LebronBoy/desktop/target/debug/deps/libgb_core-26e4a9770836d5d3.rlib: /Users/salabdoulaye/LebronBoy/gb_core/src/lib.rs /Users/salabdoulaye/LebronBoy/gb_core/src/gb/mod.rs /Users/salabdoulaye/LebronBoy/gb_core/src/gb/cpu.rs
+/Users/salabdoulaye/LebronBoy/desktop/target/debug/deps/libgb_core-26e4a9770836d5d3.rlib: /Users/salabdoulaye/LebronBoy/gb_core/src/lib.rs /Users/salabdoulaye/LebronBoy/gb_core/src/bus/mod.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/mod.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/registers.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/instructions.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/execute.rs
 
-/Users/salabdoulaye/LebronBoy/desktop/target/debug/deps/gb_core-26e4a9770836d5d3.d: /Users/salabdoulaye/LebronBoy/gb_core/src/lib.rs /Users/salabdoulaye/LebronBoy/gb_core/src/gb/mod.rs /Users/salabdoulaye/LebronBoy/gb_core/src/gb/cpu.rs
+/Users/salabdoulaye/LebronBoy/desktop/target/debug/deps/gb_core-26e4a9770836d5d3.d: /Users/salabdoulaye/LebronBoy/gb_core/src/lib.rs /Users/salabdoulaye/LebronBoy/gb_core/src/bus/mod.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/mod.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/registers.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/instructions.rs /Users/salabdoulaye/LebronBoy/gb_core/src/cpu/execute.rs
 
 /Users/salabdoulaye/LebronBoy/gb_core/src/lib.rs:
-/Users/salabdoulaye/LebronBoy/gb_core/src/gb/mod.rs:
-/Users/salabdoulaye/LebronBoy/gb_core/src/gb/cpu.rs:
+/Users/salabdoulaye/LebronBoy/gb_core/src/bus/mod.rs:
+/Users/salabdoulaye/LebronBoy/gb_core/src/cpu/mod.rs:
+/Users/salabdoulaye/LebronBoy/gb_core/src/cpu/registers.rs:
+/Users/salabdoulaye/LebronBoy/gb_core/src/cpu/instructions.rs:
+/Users/salabdoulaye/LebronBoy/gb_core/src/cpu/execute.rs:
